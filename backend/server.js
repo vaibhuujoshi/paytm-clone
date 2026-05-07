f23f571cc36ccc37d3007cd59b5c222a328bfd0b
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-05-07 19:30:56 +0530

    Add: Routing

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
+import connectDB from "./config/db";
+import rootRouter from "./routes/index.js"
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/v1/user', rootRouter);
+
+connectDB();
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
