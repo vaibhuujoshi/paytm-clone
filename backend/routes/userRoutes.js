40c584e525fa84c79bc38cfd00394e0ab6fedac8
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-05-07 20:36:45 +0530

    Add: user signIn signup routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,120 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import { z } from "zod";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import UserModel from "../models/user.js";
 
 const router = express.Router();
+dotenv.config();
 
-router.post('/signup');
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post('/signup', async (req, res) => {
+    const { username, password, firstName, lastName } = req.body;
+
+    const requiredBody = z.object({
+        username: z.string().min(3).max(30),
+        password: z.string()
+            .min(8, { message: "Password should have minimum length of 8" })
+            .max(15, "Password is too long")
+            .regex(/^(?=.*[A-Z]).{8,}$/, {
+                message:
+                    "Should Contain at least one uppercase letter and have a minimum length of 8 characters.",
+            }),
+        firstName: z.string().min(1).max(50),
+        lastName: z.string().min(1).max(50),
+    });
+
+    const parsedWithSuccess = requiredBody.safeParse(req.body);
+
+    if (!parsedWithSuccess) {
+        return res.status(411).json({
+            message: "invalid format"
+        })
+    }
+
+    const existingUser = await UserModel.findOne({
+        username: username
+    });
+
+    if (existingUser) {
+        return res.status(411).json({
+            message: "username already exists"
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await UserModel.create({
+        username,
+        password: hashedPassword,
+        firstName,
+        lastName
+    })
+
+    res.status(200).json({
+        message: "You are signed up successfully"
+    })
+});
+
+router.post('/signin', async (req, res) => {
+    const { username, password } = req.body;
+
+    const requiredBody = z.object({
+        username: z.string().min(3).max(30),
+        password: z.string()
+            .min(8, { message: "Password should have minimum length of 8" })
+            .max(15, "Password is too long")
+            .regex(/^(?=.*[A-Z]).{8,}$/, {
+                message:
+                    "Should Contain at least one uppercase letter and have a minimum length of 8 characters.",
+            }),
+    });
+
+    const parsedWithSuccess = requiredBody.safeParse(req.body);
+
+    if (!parsedWithSuccess) {
+        return res.status(411).json({
+            message: "invalid format"
+        })
+    }
+
+    const user = await UserModel.findOne({
+        username: username
+    });
+
+    if (!user) {
+        return res.status(411).json({
+            message: "user not found"
+        })
+    }
+
+    const matchPassword = await bcrypt.compare(password, user.password);
+
+    if (!matchPassword) {
+        return res.status(403).json({
+            message: "Invalid Credentials"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id
+    }, JWT_SECRET,
+        { expiresIn: "1h" }
+    );
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 60 * 60 * 1000
+    })
+
+    return res.status(200).json({
+        token: token,
+        message: "You are signed in successfully"
+    })
+});
 
 export default router;
