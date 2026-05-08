abbcf7e5b1d20c52a708a1499a915a93c1b24264
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-05-08 17:08:36 +0530

    Add: cors and cookie-parser middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import connectDB from "./config/db";
 import rootRouter from "./routes/index.js"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use('/api/v1/user', rootRouter);
 
