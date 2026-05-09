0877ffbb8dedea160f52306a076f25acdc57fdca
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-05-09 23:11:46 +0530

    Fix: post data to backend

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,19 +1,49 @@
+import { useState } from "react";
 import BottomWarning from "../components/BottomWarning";
 import Button from "../components/Button";
 import Heading from "../components/Heading";
 import InputBox from "../components/InputBox";
-import { SubHeading } from "../components/SubHeading";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    async function signup() {
+        const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
+            username,
+            password,
+            firstName,
+            lastName
+        });
+
+        if (response.status === 200) {
+            navigate('/signin');
+        }
+    }
+
     return (
         <div className="flex flex-col gap-4 items-center justify-center px-4 py-2 md:px-8 md:py-4 md:w-96 rounded-xl shadow-2xl bg-slate-950/15 border border-mariner-700/50 backdrop-blur-md mt-3">
             <Heading label={"Sign Up"} />
             {/* <SubHeading label={"Enter your information to create an account"} /> */}
-            <InputBox label={"First Name"} placeholder={"John"} onChange={""} />
-            <InputBox label={"Last Name"} placeholder={"Doe"} onChange={""} />
-            <InputBox label={"Username"} placeholder={"johndoe"} onChange={""} />
-            <InputBox label={"Password"} placeholder={""} onChange={""} />
-            <Button label={'Sign Up'} onClick={""} />
+            <InputBox label={"First Name"} type={"text"} placeholder={"John"} onChange={e => {
+                setFirstName(e.target.value)
+            }} />
+            <InputBox label={"Last Name"} type={"text"} placeholder={"Doe"} onChange={e => {
+                setLastName(e.target.value)
+            }} />
+            <InputBox label={"Username"} type={"text"} placeholder={"johndoe"} onChange={e => {
+                setUsername(e.target.value)
+            }} />
+            <InputBox label={"Password"} type={"password"} placeholder={".........."} onChange={e => {
+                setPassword(e.target.value)
+            }} />
+            <Button label={'Sign Up'} onClick={signup} />
             <BottomWarning label={'Already have an account?'} buttonText={'Login'} to={'/signin'} />
         </div>
     )
