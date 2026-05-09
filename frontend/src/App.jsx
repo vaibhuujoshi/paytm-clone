ef30b328fd5f02465b3c609012d6fb982c5b66e5
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-05-09 16:08:24 +0530

    Add: Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Dashboard from './pages/Dashboard';
+import SendMoney from './pages/SendMoney';
+import Appbar from './components/Appbar';
 
 function App() {
-  
+    return (
+        <div className='bg-linear-to-b from-black to-[#001831] w-full min-h-screen text-mariner-50 flex flex-col p-8 md:p-16 font-inter md:items-center'>
+            <BrowserRouter>
+            <Appbar />
+                <Routes>
+                    <Route path='/signup' element={<Signup />} />
+                    <Route path='/signin' element={<Signin />} />
+                    <Route path='/dashboard' element={<Dashboard />} />
+                    <Route path='/send' element={<SendMoney />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App
