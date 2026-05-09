6d31b5c84840245038a324f19e79c25bdbf00fcc
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-05-09 23:08:39 +0530

    Fix: data from backend

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+import Avatar from "./Avatar";
+import axios from "axios";
+
 export default function Appbar() {
+  const [name, setName] = useState("User");
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/v1/user/me', { withCredentials: true })
+      .then(response => setName(response.data.firstName));
+  })
+
   return (
     <nav className="fixed top-0 w-full z-50 bg-black/20 backdrop-blur-lg border-b border-white/10">
-      <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
+      <div className="max-w-7xl mx-auto px-4 h-16 flex items-center justify-between">
 
         <div className="text-xl font-bold text-mariner-50 tracking-tight">
           Pay<span className="text-mariner-500">TM</span>
@@ -9,13 +20,9 @@ export default function Appbar() {
 
         <div className="flex items-center">
             <div className="flex flex-col justify-center h-full mr-4">
-                Hello, User
+                Hello, {name}
             </div>
-          <div className="bg-mariner-600 hover:bg-mariner-500 rounded-full text-white h-12 w-12 text-center text-sm font-semibold transition-all cursor-pointer">
-            <h1 className="flex flex-col justify-center h-full text-xl">
-                    U
-                </h1>
-          </div>
+          <Avatar label={name[0]} />
         </div>
       </div>
     </nav>
