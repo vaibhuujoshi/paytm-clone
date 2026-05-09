d1dc896d0b767525f75d4484f1255d4bd10c4232
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-05-10 01:28:40 +0530

    Fix: fallback to signin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import './App.css';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
@@ -12,6 +12,7 @@ function App() {
         <div className='bg-linear-to-b from-black to-[#001831] w-full min-h-screen text-mariner-50 flex flex-col p-8 md:p-16 font-inter md:items-center'>
             <BrowserRouter>
                 <Routes>
+                    <Route path="/" element={<Navigate to="/dashboard" replace />} />
                     <Route path='/signup' element={<Signup />} />
                     <Route path='/signin' element={<Signin />} />
                     <Route path='/dashboard' element={<Dashboard />} />
